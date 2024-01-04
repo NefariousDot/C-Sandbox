ead5d76ce4cba3945de13d86fecd77cb07393afd
NefariousDot <86283530+NefariousDot@users.noreply.github.com>
2024-01-04 22:11:13 +0530

    Update triangles.c

--- C_Learnigs/triangles.c
+++ C_Learnigs/triangles.c
@@ -12,6 +12,56 @@ void leftAsterix(int row){
     }
 }
 
+void rightAsterisk(int row){
+    int i,j;
+    for(i=1;i<=row;i++){
+        //printing spaces
+        for(j=1;j<=(row-i);j++){
+            printf("  ");
+        }
+        //printing asterisk
+        for(j=1;j<=i;j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
+
+void downLeftAsterisk(int row){
+    int i,j;
+    for(i=0;i<row;i++){
+        for(j=1;j<=row-i;j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
+
+void downRightAsterisk(int row){
+    int i,j;
+    for(i=0;i<row;i++){
+        for(j=1;j<=i;j++){
+            printf("  ");
+        }
+        for(j=1;j<=row-i;j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
+
+void floydTriangle(int row){
+    int i,j;
+    int count =1;
+    for(i=1;i<=row;i++){
+        for(j=1;j<=i;j++){
+            printf("%d ", count);
+            count ++;
+        }
+        printf("\n");
+    }
+}
+
 int main()
 {
     int row;
@@ -19,6 +69,15 @@ int main()
     scanf("%d",&row);
 
     leftAsterix(row);
+    printf("\n\n");
+    rightAsterisk(row);
+    printf("\n\n");
+    downLeftAsterisk(row);
+    printf("\n\n");
+    downRightAsterisk(row);
+    printf("\n\n");
+    floydTriangle(row);
+    printf("\n\n");
     
     return 0;
 }
