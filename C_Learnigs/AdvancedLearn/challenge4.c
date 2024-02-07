19bd5de21f48d5aba3b394341bb7cb2b65495cc9
athmaram <athmaram@cdac.in>
2024-02-07 18:00:52 +0530

    New File

--- C_Learnigs/AdvancedLearn/challenge4.c
+++ C_Learnigs/AdvancedLearn/challenge4.c
@@ -19,3 +19,76 @@ some default vale and print those values
 * SECOND;modify the structure content by accessing each bitfield and changing the
 default value to another value, then print the modified value 
  **************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef unsigned int UI;
+typedef unsigned short US;
+typedef unsigned char UC;
+
+/*LINE STYLES*/
+#define SOLID   0
+#define DOTTED  1
+#define DASHED  2
+
+/*PRIMARY COLORS*/
+#define RED     1 
+#define GREEN   2
+#define BLUE    4
+
+/*MIXED COLORS*/
+#define BLACK   0
+#define YELLOW  (RED | GREEN)
+#define MAGENTA (RED | BLUE)
+#define CYAN    (GREEN | BLUE)
+#define WHITE   (RED | GREEN | BLUE )
+
+const UC * color[8] = {"black", "red", "green", "yellow", "blue", "magenta", "cyan", "white"};
+
+typedef struct onScreenBox{
+    UI :4;
+    US lucid:1;
+    UI fill_color:3;
+    UI :2;
+    US boarder_type:1;
+    UI boarder_color:3;
+    US boarder_style:2;
+}screenBox;
+
+void showBox(screenBox *ptr){
+    printf("The box is %s\n",(ptr->lucid == 1)? "transparent" : "opaque");
+    printf("Fill color is %s\n",color[ptr->fill_color]);
+    printf("Boarder Type is %s\n",(ptr->boarder_type ? "hidden" : "shown"));
+    printf("Boarder Color : %s\n", color[ptr->boarder_color]);
+    printf("Boarder style is ");
+    switch (ptr->boarder_style)
+    {
+    case SOLID : printf("solid\n");
+        break;
+    
+    case DOTTED : printf("dotted\n");
+        break;
+
+    case DASHED : printf("dashed\n");
+        break;
+    
+    default: printf("Unknown");
+        break;
+    }
+     
+}
+
+
+int main(){
+    screenBox box = {0, MAGENTA, 1, GREEN, DOTTED};
+
+    printf("\n***************************************************\n");
+    printf("INITIAL BOX DETAILS \n");
+    printf("***************************************************\n");
+    showBox(&box);
+    printf("\n");
+
+    return 0;
+}
+
